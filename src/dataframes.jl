193b990808f146bf725f7df762020b53bb938c4a
Truls Flatberg <Truls.Flatberg@sintef.no>
2021-12-22 15:44:58 +0100

    Expand additional period types

--- src/dataframes.jl
+++ src/dataframes.jl
@@ -5,6 +5,12 @@ function expand_dataframe(df::DataFrame)
         if eltype(col) == TreeNode
             df[!,:strategic_period] = [strat_per(t) for t in col] 
             df[!,:branch] = [branch(t) for t in col] 
+        elseif eltype(col) == OperationalPeriod
+            df[!,:strategic_period] = [strat_per(t) for t in col] 
+            df[!,:op_period] = [t.op for t in col] 
+        elseif eltype(col) == SimplePeriod
+            df[!,:strategic_period] = [1 for t in col] 
+            df[!,:op_period] = [t.op for t in col]     
         end
     end
     return df
