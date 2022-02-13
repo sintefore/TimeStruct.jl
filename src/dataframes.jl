e4c9626615edb6a6949a77f49c75e10eac251534
Truls Flatberg <Truls.Flatberg@sintef.no>
2022-02-13 12:48:43 +0100

    Fix dataframe expanding

--- src/dataframes.jl
+++ src/dataframes.jl
@@ -1,17 +1,18 @@
 using .DataFrames
 
-function expand_dataframe(df::DataFrame)
+function expand_dataframe!(df::DataFrame)
     for col in eachcol(df)
-        if eltype(col) == TreeNode
+        if eltype(col) <: StratNode
             df[!,:strategic_period] = [strat_per(t) for t in col] 
             df[!,:branch] = [branch(t) for t in col] 
-        elseif eltype(col) == OperationalPeriod
+        elseif eltype(col) <: OperationalPeriod
             df[!,:strategic_period] = [strat_per(t) for t in col] 
-            df[!,:op_period] = [t.op for t in col] 
-        elseif eltype(col) == SimplePeriod
+            df[!,:oper_period] = [t.op for t in col] 
+        elseif eltype(col) <: SimplePeriod
             df[!,:strategic_period] = [1 for t in col] 
-            df[!,:op_period] = [t.op for t in col]     
+            df[!,:oper_period] = [t.op for t in col]     
+        elseif eltype(col)  <: StrategicPeriod
+            df[!,:strategic_period] = [strat_per(t) for t in col] 
         end
     end
-    return df
 end
