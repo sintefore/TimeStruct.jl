9791b3dceada2bf826bea59d0c89df22668f004c
Truls Flatberg <75753981+trulsf@users.noreply.github.com>
2024-01-09 22:30:29 +0100

    Update github actions and remove gitlab (#1)

--- coverage/coverage-summary.jl
+++
@@ -1,8 +0,0 @@
-using Coverage
-
-cd(joinpath(@__DIR__, "..")) do
-    processed = process_folder()
-    covered_lines, total_lines = get_summary(processed)
-    percentage = covered_lines / total_lines * 100
-    return println("($(percentage)%) covered")
-end
