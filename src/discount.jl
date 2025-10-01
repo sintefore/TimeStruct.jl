ab21c13b7c05e1db5365ebcf69680d71d8b56c3b
Julian Straus <julian.straus@sintef.no>
2025-10-01 10:40:04 +0200

    Restructured discounting approach

--- src/discount.jl
+++ src/discount.jl
@@ -1,3 +1,5 @@
+DiscPeriods = Union{TimePeriod,AbstractOperationalScenario,AbstractRepresentativePeriod}
+
 """
     Discounter(discount_rate, timeunit_to_year, ts)
 
@@ -8,15 +10,12 @@ struct Discounter
     timeunit_to_year::Any
     ts::TimeStructure
 end
-
 Discounter(rate, ts) = Discounter(rate, 1.0, ts)
 
 _start_strat(sp::AbstractStrategicPeriod, ts::TimeStructure{T}) where {T} = zero(T)
-
 function _start_strat(sp::AbstractStrategicPeriod, ts::TwoLevel{S}) where {S}
     return sum(duration_strat(spp) for spp in strat_periods(ts) if spp < sp; init = zero(S))
 end
-
 function _start_strat(sp::StratNode, ts::TwoLevelTree{S}) where {S}
     start = zero(S)
     node = _parent(sp)
@@ -26,11 +25,9 @@ function _start_strat(sp::StratNode, ts::TwoLevelTree{S}) where {S}
     end
     return start
 end
-
-DiscPeriods = Union{TimePeriod,AbstractOperationalScenario,AbstractRepresentativePeriod}
-
 _start_strat(t::DiscPeriods, ts::TimeStructure) = _start_strat(_sp_period(t, ts), ts)
 
+_sp_period(sp::AbstractStrategicPeriod, _::TimeStructure) = sp
 function _sp_period(t::DiscPeriods, ts::TimeStructure)
     for sp in strat_periods(ts)
         if _strat_per(sp) == _strat_per(t)
@@ -39,7 +36,6 @@ function _sp_period(t::DiscPeriods, ts::TimeStructure)
     end
     @error("Time period not part of any strategic period")
 end
-
 function _sp_period(t::TreePeriod, tree::TwoLevelTree)
     for sp in strat_periods(tree)
         if _strat_per(sp) == _strat_per(t) && _branch(sp) == _branch(t)
@@ -67,19 +63,35 @@ The `timeunit_to_year` parameter is used to convert the time units of
 strategic periods in the time structure to years (default value = 1.0).
 """
 function discount(
-    t::DiscPeriods,
+    t::Union{TimePeriod,TimeStructurePeriod},
     ts::TimeStructure,
     discount_rate;
     type = "start",
     timeunit_to_year = 1.0,
 )
     sp = _sp_period(t, ts)
-
-    return discount(sp, ts, discount_rate; type, timeunit_to_year)
+    discount_factor = 1.0
+    if discount_rate > 0
+        start_year = _to_year(_start_strat(sp, ts), timeunit_to_year)
+        duration_years = _to_year(duration_strat(sp), timeunit_to_year)
+        if type == "start"
+            discount_factor = discount_start(discount_rate, start_year)
+        elseif type == "avg"
+            discount_factor = discount_avg(discount_rate, start_year, duration_years)
+        elseif type == "avg_year"
+            discount_factor = discount_avg_year(discount_rate, start_year, duration_years)
+        end
+    end
+    return discount_factor
 end
-
-function discount(disc::Discounter, t::TimePeriod; type = "start", timeunit_to_year = 1.0)
-    return discount(t, disc.ts, disc.discount_rate; type, timeunit_to_year)
+function discount(
+    disc::Discounter,
+    t::Union{TimePeriod,TimeStructurePeriod};
+    type = "start",
+)
+    ts = disc.ts
+    timeunit_to_year = disc.timeunit_to_year
+    return discount(_sp_period(t, ts), ts, disc.discount_rate; type, timeunit_to_year)
 end
 
 function discount_avg(discount_rate, start_year, duration_years)
@@ -98,28 +110,6 @@ function discount_start(discount_rate, start_year)
     return δ^start_year
 end
 
-function discount(
-    sp::AbstractStrategicPeriod,
-    ts::TimeStructure,
-    discount_rate;
-    type = "start",
-    timeunit_to_year = 1.0,
-)
-    discount_factor = 1.0
-    if discount_rate > 0
-        start_year = _to_year(_start_strat(sp, ts), timeunit_to_year)
-        duration_years = _to_year(duration_strat(sp), timeunit_to_year)
-        if type == "start"
-            discount_factor = discount_start(discount_rate, start_year)
-        elseif type == "avg"
-            discount_factor = discount_avg(discount_rate, start_year, duration_years)
-        elseif type == "avg_year"
-            discount_factor = discount_avg_year(discount_rate, start_year, duration_years)
-        end
-    end
-    return discount_factor
-end
-
 """
     objective_weight(t, time_struct, discount_rate; type, timeunit_to_year)
 
@@ -135,43 +125,40 @@ The `timeunit_to_year` parameter is used to convert the time units of
 strategic periods in the time structure to years (default value = 1.0).
 """
 function objective_weight(
-    t::DiscPeriods,
+    t::Union{TimePeriod,TimeStructurePeriod},
     ts::TimeStructure,
     discount_rate;
     type = "start",
     timeunit_to_year = 1.0,
+)
+    return _objective_value(t, ts, discount_rate, type, timeunit_to_year)
+end
+function objective_weight(
+    t::Union{TimePeriod,TimeStructurePeriod},
+    disc::Discounter;
+    type = "start",
+)
+    timeunit_to_year = disc.timeunit_to_year
+    return objective_weight(t, disc.ts, disc.discount_rate; type = type, timeunit_to_year)
+end
+
+function _objective_value(
+    t::DiscPeriods,
+    ts::TimeStructure,
+    discount_rate,
+    type,
+    timeunit_to_year,
 )
     return probability(t) *
            discount(t, ts, discount_rate; type, timeunit_to_year) *
            multiple(t)
 end
-
-function objective_weight(t::DiscPeriods, disc::Discounter; type = "start")
-    return objective_weight(
-        t,
-        disc.ts,
-        disc.discount_rate;
-        type = type,
-        timeunit_to_year = disc.timeunit_to_year,
-    )
-end
-
-function objective_weight(
-    sp::AbstractStrategicPeriod,
+function _objective_value(
+    t::AbstractStrategicPeriod,
     ts::TimeStructure,
-    discount_rate;
-    type = "start",
-    timeunit_to_year = 1.0,
+    discount_rate,
+    type,
+    timeunit_to_year,
 )
-    return discount(sp, ts, discount_rate; type, timeunit_to_year)
-end
-
-function objective_weight(sp::AbstractStrategicPeriod, disc::Discounter; type = "start")
-    return objective_weight(
-        sp,
-        disc.ts,
-        disc.discount_rate;
-        type = type,
-        timeunit_to_year = disc.timeunit_to_year,
-    )
+    return discount(t, ts, discount_rate; type, timeunit_to_year)
 end
